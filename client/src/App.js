89c35118ef4f96519732240a7a78bfa7b0d1dc4b
atharvadeshmukh <atharva.deshmukh@neebal.com>
2023-09-17 16:48:15 +0530

    did the navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import Navbar1 from "./components/Navbar/Index1";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"; // Uncomment this line
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      {/* Wrap your components with the Router component */}
+      <div>
+        <Navbar1 />
+
+      </div>
+    </Router>
+  );
 };
+
+// Define your route components here
+// const Home = () => <div>Home Page</div>;
 
 export default App;
