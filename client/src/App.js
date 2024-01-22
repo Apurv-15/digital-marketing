7aac6576d28cd6700d98c67f3c84414c47cc5392
atharvadeshmukh <atharva.deshmukh@neebal.com>
2024-01-22 11:18:35 +0530

    Trying Animate 3d page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { gsap, Power3 } from "gsap";
 import Content from "./components/Hero_page/HeroPage.js";
-import Images from "./components/Hero_page/Image/Image.js"; 
+import Images from "./components/Hero_page/Image/Image.js";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar/index2.js";
 import Mind from "./components/Project_in_mind/Mind.js";
 import { Info } from "./components/Info_page/Info.js";
+import Animate1 from "./components/Animated_page/Animate1.js";
+import Animate2 from "./components/Animated_page/Animate2.js";
 const App = () => {
   let tl = new gsap.timeline();
   let ease = Power3.easeOut;
@@ -14,17 +16,18 @@ const App = () => {
   return (
     <Router>
       <div>
-        <Navbar/> 
+        {/* <Navbar/>  */}
         <div className="hero-container">
-          {/* <Content timeline={tl} /> */}
-          {/* <Images timeline={tl} ease={ease} /> */}
-          
-          <Mind/>
-          {/* <Service></Service> */}
-          {/* <Info></Info> */}
+          <Content timeline={tl} />
+          <Images timeline={tl} ease={ease} />
+
+          {/* <Mind/> */}
+          {/* <Animate1 /> */}
+          {/* <Animate2 /> */}
+          <Service></Service>
+          <Info></Info>
         </div>
-        <div>
-        </div>
+        <div></div>
       </div>
     </Router>
   );
