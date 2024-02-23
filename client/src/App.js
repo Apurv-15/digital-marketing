4bd95d8d859088eeef9b9f8b564c77ab05a14bcf
atharvadeshmukh <atharva.deshmukh@neebal.com>
2024-02-23 13:29:40 +0530

    added db in res files

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,8 @@ import Product2 from "./components/Features/ProductPage2/Product2.js";
 import Product22 from "./components/Features/ProductPage2/Product22.js";
 import SignUp from "./components/Signup/Signup";
 import UserAuthContext from "./components/Auth0/UserAuthContext.js";
-import Animate1 from "./components/Animated_page/Animate1.js";
+
+// import Animate1 from "./components/Animated_page/Animate1.js";
 import Animate2 from "./components/Animated_page/Animate2.js";
 import Res1 from "./components/Features/ProductPage3/Res1.js";
 import Res2 from "./components/Features/ProductPage3/Res2.js";
@@ -20,6 +21,7 @@ import Res3 from "./components/Features/ProductPage3/Res3.js";
 import Res4 from "./components/Features/ProductPage3/Res4.js";
 import MouseTrial from "./components/MouseTrail/MouseTrail.js";
 import "./App.css";
+
 gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
@@ -28,22 +30,33 @@ const App = () => {
 
   useEffect(() => {
     // Set up ScrollTrigger animations
-    gsap.from(".animate1", {
+    gsap.from(".Content", {
       opacity: 0,
       y: 50,
       scrollTrigger: {
-        trigger: ".animate1",
+        trigger: ".Content",
         start: "top center", // Adjust as needed
         end: "bottom center", // Adjust as needed
         scrub: true, // Smooth animation while scrolling
       },
     });
 
-    gsap.from(".animate2", {
+    gsap.from(".Info", {
       opacity: 0,
       y: 50,
       scrollTrigger: {
-        trigger: ".animate2",
+        trigger: ".Info",
+        start: "top center", // Adjust as needed
+        end: "bottom center", // Adjust as needed
+        scrub: true, // Smooth animation while scrolling
+      },
+    });
+
+    // Add zoom in/out animation on scroll
+    gsap.from(".Images", {
+      scale: 1.2,
+      scrollTrigger: {
+        trigger: ".Images",
         start: "top center", // Adjust as needed
         end: "bottom center", // Adjust as needed
         scrub: true, // Smooth animation while scrolling
@@ -61,7 +74,7 @@ const App = () => {
         {/* <div className="hero-container"> */}
         <MouseTrial />
         {/* <Content timeline={tl} /> */}
-        {/* <Images timeline={tl} ease={ease} /> */}
+        {/* <Images timeline={tl} ease={ease} className="Images" /> */}
 
         {/* <Animate1 className="animate1" /> */}
         {/* <Animate2 className="animate2" /> */}
--- client/src/components/Features/ProductPage3/Res4.js
+++ client/src/components/Features/ProductPage3/Res4.js
@@ -1,11 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
+import { useNavigate, useLocation } from "react-router-dom";
 import CardMedia from "@mui/material/CardMedia";
+import { db } from "../../Firebase/firebase.config";
+import { collection, doc, setDoc } from "firebase/firestore";
 import "./Res.css";
+import { useAuth } from "../../Auth0/UserAuthContext";
 
 const Res4 = () => {
+  const [selectedCard4, setSelectedCard4] = useState(null);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { currentuser } = useAuth();
+
+  const productName1 = location.state?.productName;
+  const locationName1 = location.state?.locationName;
+  const budget1 = location.state?.budget;
+  const ageGroup1 = location.state?.ageGroup;
+  const res1_card_data = location.state?.cardData;
+
+  const handleButtonClick1 = async () => {
+    if (selectedCard4 !== null) {
+      let cardData4;
+      if (selectedCard4 === 0) {
+        cardData4 = ""; //enter value of data
+      } else if (selectedCard4 === 1) {
+        cardData4 = "";
+      } else if (selectedCard4 === 2) {
+        cardData4 = "";
+      }
+
+      try {
+        const userEmail = currentuser?.email;
+
+        const userCollectionRef = collection(db, userEmail);
+        const productDocRef = doc(userCollectionRef, productName1);
+        // setProductDocRef(doc(userCollectionRef, productName));
+
+        await setDoc(productDocRef, {
+          ageGroup: ageGroup1,
+          location: locationName1,
+          budget: budget1,
+          Advertisment_Type: res1_card_data,
+          Advertisment_Channel: cardData4col,
+          //add carddata4
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
   return (
     <>
       <div className="body">
